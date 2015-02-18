0bf8eccb7fb3bd1f3142fdf6a8e5c5c9855055a0
TylerWilcock <wilcocktl31@uww.edu>
2015-02-17 20:23:34 -0600

    Integrated new SceneBuilder interface

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 <h1> Wiki Scraper </h1>
-The goal of this project is to scrape words off random (or not random) Wikipedia pages and display them in nice looking graphs in reddit.com/r/dataisbeautiful style.  The idea for this project just came to me one day, and while not entirely useful, could end up looking pretty cool and I hope to learn several things along the way.  
+The goal of this project is to scrape words off random (or not random) Wikipedia pages and display them in nice looking graphs in reddit.com/r/dataisbeautiful style.  The idea for this project just came to me one day, and while not entirely useful, could end up looking pretty cool with the added benefit of learning some new things along the way.
--- src/main/java/main/Driver.java
+++ src/main/java/main/Driver.java
@@ -1,13 +1,17 @@
 package main;
 
+import java.io.IOException;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
  
@@ -18,35 +22,26 @@ public class Driver extends Application {
 		mainScraper.startParsing("https://en.wikipedia.org/wiki/Banana");
 		//mainScraper.startParsing("http://en.wikipedia.org/w/index.php?title=banana&action=raw");
 		//mainScraper.startParsing("http://tylernoip.ddns.net/");
-        launch(args);
+		launch(args);
     }
     
     @Override
     public void start(Stage primaryStage) {
     	
-        Button scrapeBtn = new Button();
-        scrapeBtn.setText("Scrape!");
-        scrapeBtn.setOnAction(new EventHandler<ActionEvent>() {
- 
-            public void handle(ActionEvent event) {
-                System.out.println("Scrape button pressed.");
-            }
-        });
-        
-        TextField console = new TextField();
-        
-        GridPane root = new GridPane();    
-        
-        GridPane.setConstraints(scrapeBtn, 2, 2, 1, 1);
-        root.getChildren().add(scrapeBtn);
+    	FXMLLoader loader = new FXMLLoader(getClass().getResource("interface.fxml"));
+	    Pane root=null;
+	    
+	    try {
+	        root = (Pane) loader.load();
+	    }
+	    catch (IOException e) {
+	        e.printStackTrace();
+	    }
 
-        root.getChildren().add(console);
-
-        Scene scene = new Scene(root, 600, 400);
+        Scene scene = new Scene(root);
 
         primaryStage.setTitle("Wiki Scraper");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
-
 }
