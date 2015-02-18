ee40ffe1875892ff8c187e1074a3dc60597832d4
TylerWilcock <wilcocktl31@uww.edu>
2015-02-17 20:35:20 -0600

    Changed repo from Eclipse to Netbeans project
    I was have some trouble getting things to work with my interface.fxml
    file, and decided to give Netbeans a try.  Good chance to try something
    new

---
+++ src/main/java/org/scrape/scraper/ScraperController.java
@@ -0,0 +1,17 @@
+package org.scrape.scraper;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+class ScraperController implements Initializable{
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
